3f6a4b7794837bc08ec918db4678781676d27568
stadmin <stadmin@nyc.yearup.org>
2018-11-05 11:01:50 -0500

    second part completed

--- w03/d1/morning_exercise/main.js
+++ w03/d1/morning_exercise/main.js
@@ -1,1 +1,14 @@
-// Write a program that will `console.log` the numbers from 1 to 100. But for multiples of three `console.log` “Fizz” instead of the number and for the multiples of five `console.log` “Buzz”. For numbers which are multiples of both three and five `console.log` “FizzBuzz”.
+// Write a program that will `console.log`
+//the numbers from 1 to 100. But for multiples of three `console.log` “Fizz”
+//instead of the number and for the multiples of five `console.log` “Buzz”.
+// For numbers which are multiples of both three and five `console.log` “FizzBuzz”.
+
+//function to create a loop print buzz for multiple of 5 and Fizz for multiple of 3.
+
+for(var i=1; i<101; i++){
+  if(i%3==0){
+    console.log("Fizz");
+  }else if(i%5==0){
+    console.log("Buzz");
+  }
+}
