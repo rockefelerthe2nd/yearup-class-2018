1bb89bbfcca16c471623fcbad28976ef04b9337d
stadmin <stadmin@nyc.yearup.org>
2018-11-07 15:50:43 -0500

    uploading file

---
+++ w03/d1/workshop/workshop/main.js
@@ -0,0 +1,70 @@
+// first scroll effect
+//
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//     if($(this).scrollTop()>100){
+//       $(".section_one").css({"opacity" : "0.5"})
+//       $(".nav_bar").css({"background-color" : "#da9eb5"})
+//     }
+//     else{
+//       $(".section_one").css({"opacity" : "1"})
+//       $(".nav_bar").css({"background-color" : "#f4eaee"})
+//     }
+//   })
+// })
+//
+// // second scroll effect
+//
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//     if($(this).scrollTop()>900){
+//       $(".section_three").css({"opacity" : "0.5"})
+//       $(".nav_bar").css({"background-color" : "#cc7999"})
+//     }
+//     else{
+//       $(".section_three").css({"opacity" : "1"})
+//     }
+//   })
+// })
+//
+// //third scroll effect
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//
+//     if($(this).scrollTop()>2000){
+//       $(".section_two").css({"opacity" : "0.5"})
+//       $(".nav_bar").css({"background-color" : "#d38ba7"})
+//     }
+//     else{
+//       $(".section_two").css({"opacity" : "1"})
+//     }
+//   })
+// })
+//
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//
+//     if($(this).scrollTop()>1400){
+//     $(".content_two").css({"transform" : "translateY(0px)"})
+//     }
+//     else{
+//
+//     $(".content_two").css({"transform" : "translateY(200px)"})
+//     }
+//   })
+// })
+//
+
+
+//
+// $(document).scroll(function() {
+//   //  console.log($(document).scrollTop());
+// })
+// var y = 0;
+// function myFunction() {
+//     var elmnt = document.querySelectorAll("body");
+//     y = elmnt.scrollTop;
+//     document.querySelectorAll("body").innerHTML = "Vertically: " + y + "px";
+// }
+
+console.log(myFunction());
